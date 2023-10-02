d831cc9c723a4d26abf0ee735ea1852689e92638
rooddeeo <rooddeeo@gmail.com>
2023-10-02 20:23:19 +0300

    JS Home Work 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,22 @@
-const BASE_URL = 'https://api.thecatapi.com/v1/';
-
 function fetchBreeds(BASE_URL, API_KEY) {
-  return fetch(`${BASE_URL}breeds?breed_ids=beng,abys,api_key=${API_KEY}&=`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Ошибка загрузки данных: ${response.status}`);
-      }
-      return response.json();
-    });
+  return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(`Ошибка загрузки данных: ${response.status}`);
+    }
+    return response.json();
+  });
 }
 
-function fetchCatByBreed(breedId) {
-  return fetch(`${BASE_URL}images/search?breed_ids=${breedId}`)
-    .then(response => {
+function fetchCatByBreed(BASE_URL, breedId) {
+  return fetch(`${BASE_URL}images/search?breed_ids=${breedId}`).then(
+    response => {
       if (!response.ok) {
         throw new Error(`Ошибка загрузки данных: ${response.status}`);
       }
       // console.log(response.json());
       return response.json();
-    });
+    }
+  );
 }
 
-  export{fetchBreeds, fetchCatByBreed};
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import {fetchBreeds, fetchCatByBreed} from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const BASE_URL = 'https://api.thecatapi.com/v1/';
-const API_KEY = 'live_LdyZZAkvOUOZW3p4VB6sUES1xXuWB4URiEdzXmtox4uhzgcwlKHnYfmIPsy30pkJ';
+const API_KEY =
+  'live_LdyZZAkvOUOZW3p4VB6sUES1xXuWB4URiEdzXmtox4uhzgcwlKHnYfmIPsy30pkJ';
 
 fetchBreeds(BASE_URL, API_KEY)
   .then(data => {
     console.log(data);
 
     const breedSelect = document.querySelector('.breed-select');
+
     const addBreedCats = data
-      .map(breedCat => `<option value='${breedCat.id}'>${breedCat.name}</option>`)
+      .map(
+        breedCat => `<option value='${breedCat.id}'>${breedCat.name}</option>`
+      )
       .join('');
     breedSelect.insertAdjacentHTML('beforeend', addBreedCats);
   })
@@ -22,21 +26,18 @@ function selectedValue(event) {
   const breedId = event.target.value;
   console.log(breedId);
 
+  fetchCatByBreed(BASE_URL, breedId)
+    .then(data => {
+      console.log(data);
 
-    fetchCatByBreed(breedId)
-.then(data => {
-  console.log(data);
-
-  const catInfo = document.querySelector('.cat-info');
-  const addCatInfo = data
-    .map(infoCat => `${infoCat.name}, ${infoCat.description}, ${infoCat.temperament}`)
-    .join('');
-  catInfo.insertAdjacentHTML('beforeend', addCatInfo);
-})
-.catch(err => console.log(err));
+      const catInfo = document.querySelector('.cat-info');
+      const addCatInfo = data
+        .map(
+          infoCat =>
+            `${infoCat.name}, ${infoCat.description}, ${infoCat.temperament}`
+        )
+        .join('');
+      catInfo.insertAdjacentHTML('beforeend', addCatInfo);
+    })
+    .catch(err => console.log(err));
 }
-
-
-
-
-
